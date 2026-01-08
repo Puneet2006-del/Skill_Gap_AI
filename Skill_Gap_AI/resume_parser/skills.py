d973b853fdc997cff9b13833e00c7311479d06e7
Puneet Mehra <mehrapuneet472@gmail.com>
2026-01-08 17:48:58 +0530

    Delete Skill_Gap_AI/resume_parser directory

--- Skill_Gap_AI/resume_parser/skills.py
+++
@@ -1,9 +0,0 @@
-TECHNICAL_SKILLS = [
-    "python", "sql", "machine learning", "nlp",
-    "flask", "html", "css", "javascript", "data analysis"
-]
-
-SOFT_SKILLS = [
-    "communication", "teamwork", "leadership",
-    "problem solving", "adaptability"
-]
